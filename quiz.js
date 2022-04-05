ec6cf7d9b02552cae713fc446dca7913fc764d31
Naman Jain <jnaman475@hmail.com>
2022-04-05 21:29:17 +0530

    commit

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const quizForm = document.querySelector(".quiz-form");
+const output = document.querySelector("#output");
+
+const correctAnswer = ["90°", "right angled"];
+
+function calculateScore() {
+let score = 0;
+let index = 0;
+const formResults  = new FormData(quizForm);
+for (let value of formResults.values()){
+  if (value === correctAnswer[score]){
+      score = score + 1;
+  }
+  index = index + 1;
+}
+output.innerText = "Your score is " + index;
+}
+
+submitAnswerBtn.addEventListener('click', calculateScore);
